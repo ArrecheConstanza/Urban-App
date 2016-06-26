040960b25f73e33c474590ae5d38edbacdc0beb8
iarani5 <iaranizza5@gmail.com>
2016-06-26 03:08:11 -0300

    menu mas v1

--- js/controllers/controller-home.js
+++ js/controllers/controller-home.js
@@ -1,6 +1,9 @@
 /********************************************CONTROLLER HOME***************************************/
 
 Urban.controller("homeCtrl", function ($scope,$http) { 
-
+	id("masMenu").style.display="none";
+	id("menuMas").onclick=function(){
+		id("masMenu").style.display="inline";
+	}
 });
 
