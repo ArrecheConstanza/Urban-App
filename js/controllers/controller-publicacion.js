e10f89d38d8a931b6626c0b94ecceae2653d877b
iarani5 <iaranizza5@gmail.com>
2016-06-29 00:40:30 -0300

    arreglo nueva publicacion

--- js/controllers/controller-publicacion.js
+++ js/controllers/controller-publicacion.js
@@ -14,6 +14,7 @@ Urban.controller("newPublicacionCtrl", function ($scope, $http, $location, Uploa
 	footer.style.display="none";
 	//funcion volver atras
 	$scope.$back = function() { 
+		window.localStorage.removeItem("publi_edit");
 		header.style.display="inline";
 		footer.style.display="inline";
 		window.history.back();
@@ -22,6 +23,7 @@ Urban.controller("newPublicacionCtrl", function ($scope, $http, $location, Uploa
 	/////EDITAR
 	if(localStorage.getItem("publi_edit")!=null){
 		var datos_anteriores=angular.fromJson(localStorage.getItem("publi_edit"));
+		window.localStorage.removeItem("publi_edit");
 		var conteiner=id("container_form");
 		conteiner.innerHTML+="<form  role='form' enctype='multipart/form-data' name='publi'><select class='select' name='grupo'><option  value='Todos'  ng-model='GRUPO'>Todos</option><option  value='Uno'>Uno</option><option  value='Dos'>Dos</option></select><div class='col-lg-offset-10 col-md-offset-10 col-sm-offset-8 col-xs-offset-6 col-lg-2 col-md-2 col-sm-4 col-xs-6'> <input class='upload-file'  type='file' ngf-select ng-model='FILE' name='file' accept='image/*' ngf-max-size='2MB'><img ng-show='publi.FILE.$valid' ngf-src='FILE' class='thumb'></div><input type='text' id='titulo_edit' name='titulo' placeholder='titulo' class='form-control' required value='"+datos_anteriores.TITULO+"' ng-model='TITULO'><textarea id='edit_descripcion' cols='10' rows='6' name='descripcion' placeholder='descripcion' class='form-control' required ng-model='DESCRIPCION'>"+datos_anteriores.DESCRIPCION+"</textarea><input type='button' class='form-control btn btn-default' id='button_upload' value='Enviar'></form>";
 		id("button_upload").onclick=function(){
@@ -52,32 +54,24 @@ Urban.controller("newPublicacionCtrl", function ($scope, $http, $location, Uploa
 	}
 	else{
 		var conteiner=id("container_form");
-		conteiner.innerHTML+="<form  role='form' ng-submit='update()' enctype='multipart/form-data' name='publi'><select class='select' name='grupo'><option  value='Todos'  ng-model='GRUPO'>Todos</option><option  value='Uno'>Uno</option><option  value='Dos'>Dos</option></select><div class='col-lg-offset-10 col-md-offset-10 col-sm-offset-8 col-xs-offset-6 col-lg-2 col-md-2 col-sm-4 col-xs-6'> <input class='upload-file'  type='file' ngf-select ng-model='FILE' name='file' accept='image/*' ngf-max-size='2MB'><img ng-show='publi.FILE.$valid' ngf-src='FILE' class='thumb'></div><input type='text' name='titulo' placeholder='titulo' class='form-control' required ng-model='TITULO'><textarea cols='10' rows='6' name='descripcion' placeholder='descripcion' class='form-control' required ng-model='DESCRIPCION'></textarea><input type='submit' class='form-control btn btn-default' value='Enviar'></form>";
-		
-	}
-	/*$scope.GRUPOS = [
-    	{ID : "0", NOMBRE : "Todos"},
-    	{ID : "1", NOMBRE : "Grupo 1"},
-   		{ID : "2", NOMBRE : "Grupo 2"},
-   		{ID : "3", NOMBRE : "Grupo 3"}
- 	];*/
-	
-	//validar inputs en el onblur
-	var datos_new_publicacion=tn(tn(document,'form',0),'input');
-	var textarea=tn(tn(document,'form',0),'textarea');
-	for(var i=0;i<datos_new_publicacion.length;i++){
-		datos_new_publicacion[i].onblur=function(){
+		conteiner.innerHTML+="<form  role='form' enctype='multipart/form-data' name='publi'><select class='select' name='grupo'><option  value='Todos'  ng-model='GRUPO'>Todos</option><option  value='Uno'>Uno</option><option  value='Dos'>Dos</option></select><div class='col-lg-offset-10 col-md-offset-10 col-sm-offset-8 col-xs-offset-6 col-lg-2 col-md-2 col-sm-4 col-xs-6'> <input class='upload-file'  type='file' ngf-select ng-model='FILE' name='file' accept='image/*' ngf-max-size='2MB'><img ng-show='publi.FILE.$valid' ngf-src='FILE' class='thumb'></div><input type='text' id='titulo_submit' name='titulo' placeholder='titulo' class='form-control' required ng-model='TITULO'><textarea cols='10' rows='6' name='descripcion' placeholder='descripcion' class='form-control' required ng-model='DESCRIPCION' id='descripcion_submit'></textarea><input id='button_submit' type='button' class='form-control btn btn-default' value='Enviar'></form>";
+		id("button_submit").onclick=function(){
+		//validar inputs en el onblur
+		/*var datos_new_publicacion=tn(tn(document,'form',0),'input');
+		var textarea=tn(tn(document,'form',0),'textarea');
+		for(var i=0;i<datos_new_publicacion.length;i++){
+			datos_new_publicacion[i].onblur=function(){
+				validar_publicacion(this);
+			}
+		}
+		textarea[0].onblur=function(){
 			validar_publicacion(this);
 		}
-	}
-	textarea[0].onblur=function(){
-		validar_publicacion(this);
-	}
-	
-	//envio del form
-	$scope.submit = function (publicacion){
+		*/
+		//envio del form
+		//$scope.submit = function (){
 		//validar inputs en el submit
-		var titulo;
+	/*	var titulo;
 		var datos_new_publicacion=tn(tn(document,'form',0),'input');
 		var textarea=tn(tn(document,'form',0),'textarea');
 		for(var i=0;i<datos_new_publicacion.length;i++){
@@ -88,10 +82,10 @@ Urban.controller("newPublicacionCtrl", function ($scope, $http, $location, Uploa
 		}
 		validar_publicacion(textarea[0],"submit");
 		var mensaje=tn(tn(document,'form',0),'p');
-
+*/
 	 	//Guardado de datos en bdd para creacion de publicacion
-		if(!mensaje.length){
-			var union="TITULO="+titulo+"&DESCRIPCION="+textarea[0].value;
+		//if(!mensaje.length){
+			var union="TITULO="+id("titulo_submit").value+"&DESCRIPCION="+id("descripcion_submit").value;
 			//ABM: new publicacion
 			$http({
 				method: 'POST',
@@ -113,6 +107,17 @@ Urban.controller("newPublicacionCtrl", function ($scope, $http, $location, Uploa
 				//mensaje Sin conexion 
 			});
 		}
-	
-	}
+		}
 });
+	
+	//}
+		
+	//}
+	/*$scope.GRUPOS = [
+    	{ID : "0", NOMBRE : "Todos"},
+    	{ID : "1", NOMBRE : "Grupo 1"},
+   		{ID : "2", NOMBRE : "Grupo 2"},
+   		{ID : "3", NOMBRE : "Grupo 3"}
+ 	];*/
+	
+	
