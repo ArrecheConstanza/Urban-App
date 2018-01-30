0cc1748f63dd6f52d93252615ac2db0120b6df47
iarani5 <iaranizza5@gmail.com>
2018-01-30 01:14:48 -0300

    .

--- js/controllers/controller-editar-datos-usuario.js
+++ js/controllers/controller-editar-datos-usuario.js
@@ -7,14 +7,26 @@ Urban.controller("editarDatosUsuarioCtrl",  ['$scope', '$http', '$location', 'Up
 	for(var i=0;i<inputs.length;i++){
 		if(inputs[i].name!=$scope.titulo.toLowerCase()&&inputs[i].value!="Guardar"){
 			inputs[i].style.display="none";
+			
 		}
 	}
+	console.log($scope.titulo);
 	if($scope.titulo.toLowerCase()=='edad'){
 		var label=ce("label");
 		label.innerHTML="Edad";
 		ac(id("form-ingreso"),label);
 		id("form-ingreso").insertBefore(label, id("form-ingreso").childNodes[0]);
 	}
+	else if($scope.titulo=='Direccion estado'){
+		var label=ce("label");
+		label.innerHTML="Dirección estado";
+		ac(id("form-ingreso"),label);
+		console.log(id("form-ingreso").childNodes[0]);
+		//id("form-ingreso").insertBefore(label, id("form-ingreso").childNodes[0]); 
+	}
+	else if($scope.titulo!='Direccion estado'){
+		tn(id("form-ingreso"),'ui-switch',0).style.display="none";
+	} 
 
 	
 	/**** Envio de form ****/
