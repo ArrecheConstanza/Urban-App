8f0551be4365247a9be80090b2840941479eb21a
iarani5 <iaranizza5@gmail.com>
2018-02-21 21:21:27 -0300

    LISTO. admin edita usuarios

---
+++ php/abm/usuario.editar.admin.php
@@ -0,0 +1,43 @@
+<?php
+	//header("Access-Control-Allow-Origin: *");
+	
+	/****** Clases *****/
+	
+	require_once('../config.php');
+	require_once('../funciones.php');
+	require_once('../clases/DBcnx.php');
+	require_once('../clases/Usuario.php');
+	require_once('../clases/Validacion.php');
+	
+	/***** Validacion ******/
+	
+	$reglas = [
+		'EMAIL' => 'required|email',
+		'CLAVE' => 'required|clave',
+		'NOMBRE' => 'required|nombre',
+		'APELLIDO' => 'required|nombre',
+		'EDAD' => 'required|date'
+	];
+	
+	
+	// falta validar datos
+	
+	if(isset($_SESSION["s_id"])){
+		$usuario = new Usuario();
+		$arrayFinal=[];
+		foreach ($_POST as $key => $value){
+			if($key!="ID"){
+				$array=[
+					"VALOR" => $value,
+					"ID" => $_POST["ID"],
+				];
+				$rta=$usuario->editar_usuario($key,$array);
+				if(!$rta){ return 0; } //error al editar
+			}
+		}
+		return 1;
+	}
+	else{ //no logueado
+		return 0;
+	}
+?>
