4e365953ead4135e1653ccd87a4e62a260d4e38d
iarani5 <iaranizza5@gmail.com>
2018-02-23 03:47:52 -0300

    .

--- js/controllers/controller-panel-de-control.js
+++ js/controllers/controller-panel-de-control.js
@@ -7,4 +7,109 @@ Urban.controller("panelDeControlCtrl", function ($scope,$http){
 	}
 	
 	$scope.listado=["Usuarios", "Grupos", "Publicaciones", "Encuestas", "Estadisticas"];
+	
+	
+	//********************** USUARIOS 
+	
+		var usuarios;
+		$http({ 
+			method:"POST",
+			url:"php/abm/traer.usuarios.php",
+			headers: {'Content-Type': 'application/x-www-form-urlencoded'}  
+		})
+		.success(function(data, status){
+			usuarios=data.length;
+			//************************* GRUPOS 
+				var grupos;
+				$http({ 
+					method:"POST",
+					url:"php/abm/traer.grupos.php",
+					headers: {'Content-Type': 'application/x-www-form-urlencoded'}  
+				})
+				.success(function(data, status){
+					grupos=data.length;
+					//************************* PUBLICACIONES
+						var publicaciones;
+						$http({ 
+							method:"POST",
+							url:"php/abm/traer.publicaciones.php",
+							headers: {'Content-Type': 'application/x-www-form-urlencoded'}  
+						})
+						.success(function(data, status){
+							publicaciones=data.length;
+							//************************* ENCUESTAS
+		
+							var encuestas;
+							$http({ 
+								method:"POST",
+								url:"php/abm/traer.encuestas.php",
+								headers: {'Content-Type': 'application/x-www-form-urlencoded'}  
+							})
+							.success(function(data, status){
+								encuestas=data.length;
+								
+								
+								/****Grafico****/
+								 new Chart(document.getElementById("bar-chart-horizontal"), {
+									type: 'horizontalBar',
+									data: {
+									  labels: ["Usuarios", "Grupos", "Publicaciones", "Encuestas"],
+									  datasets: [
+										{
+										  label: "Cantidad",
+										  backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9"],
+										  data: [usuarios,grupos,publicaciones,encuestas]
+										}
+									  ]
+									},
+									options: {
+									  legend: { display: false },
+									  title: {
+										display: true,
+										text: 'Cantidad'
+									  }
+									}
+								});
+							})
+							.error(function(){
+								
+							});
+						})
+						.error(function(){
+							
+						});
+				})
+				.error(function(){
+					
+				});
+		})
+		.error(function(){
+			//mensaje Sin conexion 
+		});
+	
+	/*
+		console.log($scope.usuarios);
+		console.log(grupos);
+		
+		/* new Chart(document.getElementById("bar-chart"), {
+			type: 'bar',
+			data: {
+			  labels: ["Usuarios", "Grupos", "Publicaciones", "Encuestas"],
+			  datasets: [
+				{
+				  label: "Cantidad",
+				  backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9"],
+				  data: [usuarios.length,grupos.length,publicaciones.length,encuestas.length]
+				}
+			  ]
+			},
+			options: {
+			  legend: { display: false },
+			  title: {
+				display: true,
+				text: 'Cantidad'
+			  }
+			}
+		}); */
+		
 });
