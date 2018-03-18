60b441c68e72e76b20b7d1f4bfa0dcb388adb30c
iarani5 <iaranizza5@gmail.com>
2018-03-18 20:26:51 -0300

    LISTO. alarma principal animacion

--- js/controllers/controller-alarma.js
+++ js/controllers/controller-alarma.js
@@ -2,15 +2,15 @@
 
 Urban.controller("alarmaCtrl", function ($location,$http,$scope,$timeout,$interval) {
 	
-	
+	id("fonto_boton").style.display="none";
 	// mantener presionado 3s
 
 	var myBtn= document.getElementById('boton_alarma');       
 	var timer, timePressed = 0;
-
 	myBtn.addEventListener('touchstart', function(e){
 			timePressed = 0;
 			timer = setInterval(function(){
+				id("fonto_boton").style.display="block";
 				timePressed += 100;
 				if (timePressed > 3000){
 				   alert('Largo click!');
@@ -20,6 +20,7 @@ Urban.controller("alarmaCtrl", function ($location,$http,$scope,$timeout,$interv
 		});
 
     myBtn.addEventListener('touchend', function(e){
+		id("fonto_boton").style.display="none";
         clearInterval(timer);
     });
 
