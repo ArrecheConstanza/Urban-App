a1be4893a9502c5f172dd3914212f4bdf27e7afc
iarani5 <iaranizza5@gmail.com>
2018-03-20 16:46:17 -0300

    LISTO. cuenta regresiva, progress bar

--- js/controllers/controller-alarma.js
+++ js/controllers/controller-alarma.js
@@ -1,9 +1,58 @@
 /********************************************CONTROLLER SIDEBAR ***************************************/
 
 Urban.controller("alarmaCtrl", function ($location,$http,$scope,$timeout,$interval) {
+
+	id("alarma_activa").style.display="none";
+
+	//ALARMA ACTIVA 
+	function alarma_activa(){
 	
-	id("fonto_boton").style.display="none";
+	//cronometro
+	   var timeLimit = 5; //tiempo en minutos
+	   var conteo = new Date(timeLimit * 60000);
+	   function inicializar(){
+		  document.getElementById('cuenta').childNodes[0].nodeValue = 
+					  conteo.getMinutes() + ":" + conteo.getSeconds();
+	   }
+		
+	   function cuenta(){
+	//progress bar
+		  var elem = document.getElementById("myBar");   
+		  var width = 1;
+		  var id = setInterval(frame, 3000);
+		  function frame() {
+			if (width >= 100) {
+			  clearInterval(id);
+			} else {
+			  width++; 
+			  elem.style.width = width + '%'; 
+			}
+		  }
+		  
+		  intervaloRegresivo = setInterval(
+			function regresiva(){
+			  if(conteo.getTime() > 0){
+				 conteo.setTime(conteo.getTime() - 1000);
+			  }
+			  else{
+				 clearInterval(intervaloRegresivo);
+				 alert("Fin");
+			  }
+			  var segundos=conteo.getSeconds();
+			  if(segundos<=9){
+				segundos="0"+segundos;
+			  }
+			  document.getElementById('cuenta').childNodes[0].nodeValue = conteo.getMinutes() + ":" + segundos;
+		   } 
+		  , 1000);
+	   }
+		cuenta();
+		onload = inicializar;  
+	}
+	
 	// mantener presionado 3s
+	id("fonto_boton").style.display="none";
+	id("alarma_activa").style.display="inline-block";
 
 	var myBtn= document.getElementById('boton_alarma');       
 	var timer, timePressed = 0;
@@ -13,8 +62,14 @@ Urban.controller("alarmaCtrl", function ($location,$http,$scope,$timeout,$interv
 				id("fonto_boton").style.display="block";
 				timePressed += 100;
 				if (timePressed > 3000){
-				   alert('Largo click!');
 				   clearInterval(timer);
+				   id("fonto_boton").style.display="none";
+				   id("alarma_inactiva").style.display="none";
+				   id("title-container").style.marginBottom="1em";
+				   id("alarma_activa").style.display="inline-block";
+				   
+				   //ALARMA ACTIVA 
+				   alarma_activa();
 				}
 			}, 100);
 		});
