13dc0e8ef07e712b7118a7fdb2779ca239c24a1b
iarani5 <iaranizza5@gmail.com>
2018-04-04 19:45:05 -0300

    .

---
+++ js/modulos/modulos_chat.js
@@ -0,0 +1,17 @@
+/******************************************MODULOS*********************************************/
+
+var Urban = angular.module('Urban', [
+  'ngRoute', //rutas
+  'mobile-angular-ui', //angular
+  'mobile-angular-ui.gestures', //gestos 
+  'ngFileUpload', //upload imagenes
+  'uiGmapgoogle-maps', //mapa
+ // 'chart.js' // para estadisticas
+])
+
+/////ADAPTACION DE PANTALLA
+
+Urban.run(function($transform) {
+  window.$transform = $transform;
+});
+
